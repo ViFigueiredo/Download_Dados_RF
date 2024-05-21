111a5197ea7dcac53dfbe3f19b010c02c8011061
Vi Figueiredo <vifigcode@gmail.com.br>
2024-05-21 18:00:42 -0300

    coleta de data de atualização

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-const getLinks = require('./services/GetLinks');
+const fs = require('fs');
+const { getLinks, askUser } = require('./services/GetLinks');
 const askFilesToDownload = require('./services/TerminalQuestions');
 const downloadFile = require('./services/DownloadFile');
 const { initDirs, unzipFile, addCSVExtension } = require('./services/UnzipFiles');
@@ -12,32 +13,83 @@ const baseURL = 'https://dados.rfb.gov.br/CNPJ/';
 initDirs();
 
 // Inicia o processo de obtenção de links, download e extração
-getLinks(baseURL)
-  .then(() => getLinks(baseURL + 'regime_tributario/'))
-  .then(async (links) => {
-    const filesToDownload = await askFilesToDownload(links);
-    for (const link of filesToDownload) {
-      const fileName = link.split('/').pop();
-      const downloadPath = `./arquivos-zip/${fileName}`;
+// let allLinks = [];
+// getLinks(baseURL)
+//   .then((links) => {
+//     allLinks = allLinks.concat(links);
+//     return getLinks(baseURL + 'regime_tributario/');
+//   })
+//   .then((links) => {
+//     allLinks = allLinks.concat(links);
 
-      logger.info(`Starting download of ${fileName}`);
-      await downloadFile(link, downloadPath);
-      logger.info(`Finished download of ${fileName}`);
+//     // Escreva todos os links em um arquivo JSON na raiz do projeto
+//     fs.writeFile('./src/links.json', JSON.stringify(allLinks, null, 2), (err) => {
+//       if (err) throw err;
+//       logger.info('Arquivo salvo com sucesso!');
+//       console.log('Arquivo salvo com sucesso!');
+//     });
+//   })
+//   .then(async () => {
+//     //   const filesToDownload = await askFilesToDownload(links);
+//     //   for (const link of filesToDownload) {
+//     //     const fileName = link.split('/').pop();
+//     //     const downloadPath = `./arquivos-zip/${fileName}`;
+//     //     logger.info(`Starting download of ${fileName}`);
+//     //     await downloadFile(link, downloadPath);
+//     //     logger.info(`Finished download of ${fileName}`);
+//     //     logger.info(`Starting extraction of ${fileName}`);
+//     //     await unzipFile(downloadPath, './arquivos-csv');
+//     //     logger.info(`Finished extraction of ${fileName}`);
+//     //   }
+//     // })
+//     // .then(() => logger.info('All files downloaded and extracted successfully.'))
+//     // .then(() => addCSVExtension('./arquivos-csv'))
+//     // .then(() => logger.info('All files downloaded, extracted and renamed successfully.'))
+//     // .then(() => connectionDB())
+//     // .then(() => {
+//     //   const fileNames = getFileNames('./arquivos-csv');
+//     //   logger.info(fileNames);
+//     //   Object.keys(fileNames).forEach((fileName) => {
+//     //     importCsvToTable(fileName, fileNames);
+//     //   });
+//   })
+//   .catch((err) => logger.info(err));
 
-      logger.info(`Starting extraction of ${fileName}`);
-      await unzipFile(downloadPath, './arquivos-csv');
-      logger.info(`Finished extraction of ${fileName}`);
+async function main() {
+  const links1 = await getLinks(baseURL);
+  const links2 = await getLinks(baseURL + 'regime_tributario/');
+  const allLinks = links1.concat(links2);
+
+  let existingLinks;
+  try {
+    existingLinks = JSON.parse(fs.readFileSync('./src/links.json'));
+  } catch (err) {
+    existingLinks = [];
+    console.log(err);
+    logger.info(err);
+  }
+
+  for (const link of allLinks) {
+    const existingLink = existingLinks.find((l) => l.url === link.url);
+
+    if (existingLink) {
+      if (new Date(link.lastModified) > new Date(existingLink.lastModified)) {
+        console.log(`O link ${link.url} é mais recente.`);
+
+        const replace = await askUser();
+        if (replace) {
+          existingLink.lastModified = link.lastModified;
+        }
+      }
+    } else {
+      existingLinks.push(link);
     }
-  })
-  .then(() => logger.info('All files downloaded and extracted successfully.'))
-  .then(() => addCSVExtension('./arquivos-csv'))
-  .then(() => logger.info('All files downloaded, extracted and renamed successfully.'))
-  .then(() => connectionDB())
-  .then(() => {
-    const fileNames = getFileNames('./arquivos-csv');
-    logger.info(fileNames);
-    Object.keys(fileNames).forEach((fileName) => {
-      importCsvToTable(fileName, fileNames);
-    });
-  })
-  .catch((err) => logger.info(err));
+  }
+
+  fs.writeFile('./src/links.json', JSON.stringify(existingLinks, null, 2), (err) => {
+    if (err) throw err;
+    console.log('Arquivo salvo com sucesso!');
+  });
+}
+
+main().catch(console.error);
--- src/services/GetLinks.js
+++ src/services/GetLinks.js
@@ -1,39 +1,55 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const url = require('url');
-const retry = require('async-retry');
+const readline = require('readline');
 
-// Função para obter links de um site
-const collectedLinks = [];
-
-async function getLinks(baseUrl) {
-  const { data } = await retry(
-    async (bail) => {
-      const response = await axios.get(baseUrl);
-      if (response.status === 404 || response.status.toString().startsWith('5'))
-        bail(new Error('Failed to fetch page'));
-      return response;
-    },
-    {
-      retries: 5,
-    }
-  );
-
+async function getLinks(url) {
+  const { data } = await axios.get(url);
   const $ = cheerio.load(data);
-  const linkElements = $('a').toArray();
+  const links = [];
 
-  // Para cada link encontrado, se terminar com '.zip', adicione à lista de links coletados
-  await Promise.all(
-    linkElements.map(async (element) => {
-      const link = $(element).attr('href');
-      const fullUrl = url.resolve(baseUrl, link);
-      if (fullUrl.endsWith('.zip')) {
-        collectedLinks.push(fullUrl);
-      }
-    })
-  );
+  $('table tr').each((i, row) => {
+    // Pule o cabeçalho da tabela
+    if (i === 0) return;
 
-  return collectedLinks;
+    const cells = $(row).find('td');
+    const linkElement = $(cells[1]).find('a');
+    const lastModified = $(cells[2]).text().trim();
+    const href = linkElement.attr('href');
+
+    const link = {
+      url: href,
+      lastModified,
+    };
+
+    if (
+      link.url == '/' ||
+      link.url == 'regime_tributario/' ||
+      link.url == '/CNPJ/' ||
+      link.url == undefined ||
+      link.url.endsWith('.pdf') ||
+      link.url.endsWith('.odt')
+    )
+      return;
+
+    link.url = url + href;
+    links.push(link);
+  });
+
+  return links
 }
 
-module.exports = getLinks;
+async function askUser() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question('O link é mais recente. Você deseja substituir? (s/n) ', (answer) => {
+      rl.close();
+      resolve(answer.toLowerCase() === 's');
+    });
+  });
+}
+
+module.exports = { getLinks, askUser };
